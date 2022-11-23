1c498a5ec85732d7279dab92921ef75a8a6b06c3
ovr3 <olivervr3@gmail.com>
2022-11-23 16:31:50 +0100

    First Commit.

---
+++ hello/target/debug/deps/hello-5c38e2fb3ee6e6e5.d
@@ -0,0 +1,5 @@
+c:\Users\famly\Documents\single-threaded_web-server\hello\target\debug\deps\hello-5c38e2fb3ee6e6e5.rmeta: src\main.rs
+
+c:\Users\famly\Documents\single-threaded_web-server\hello\target\debug\deps\hello-5c38e2fb3ee6e6e5.d: src\main.rs
+
+src\main.rs:
---
+++ hello/target/debug/deps/hello-9a211e6db2511f00.d
@@ -0,0 +1,5 @@
+c:\Users\famly\Documents\single-threaded_web-server\hello\target\debug\deps\hello-9a211e6db2511f00.rmeta: src\main.rs
+
+c:\Users\famly\Documents\single-threaded_web-server\hello\target\debug\deps\hello-9a211e6db2511f00.d: src\main.rs
+
+src\main.rs:
